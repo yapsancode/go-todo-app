bae4b2c8b82372a9af62c7eb4f1ada1652dab1f9
Isyraf <isyrafsan97@gmail.com>
2025-09-26 01:09:44 +0800

    Add Gin Framework

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Hello, Go Todo App!")
+	// create router
+	f := gin.Default()
+
+	// simple test route
+	f.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "ping pong",
+		})
+	})
+
+	// start server at localhost:8080
+	f.Run(":8080")
 }
